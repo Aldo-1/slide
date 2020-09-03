44da796a4dfa188d427d397b57615110c6ae4ec6
Aldo Wanderley Costa Junior <53620227+Aldo-1@users.noreply.github.com>
2020-09-03 17:41:01 -0300

    Revert "eventos de touch adicionas"

--- js/slide.js
+++ js/slide.js
@@ -17,33 +17,22 @@ export default class Slide{
     return this.dist.finalPosition - this.dist.movement
   }
   onStart(event){
-    let movetype;
-    if(event.type === 'mousedown'){
-      event.preventDefault()
-      this.dist.startX = event.clientX
-      movetype = 'mousemove'
-    }else{
-      this.dist.startX = event.changedTouches[0].clientX
-      movetype = 'touchmove'
-    }
-    this.wrapper.addEventListener(movetype, this.onMove);
+    event.preventDefault()
+    this.dist.startX = event.clientX
+    this.wrapper.addEventListener('mousemove', this.onMove);
   }
   onMove(event){
-    const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX
-    const finalPosition = this.updatePosition(pointerPosition)
+    const finalPosition = this.updatePosition(event.clientX)
     this.moveSlide(finalPosition)
   }
   
   onEnd(event){
-    const moveType = (event.type === 'mouseup'  )?'mousemove' : 'touchmove'
-    this.wrapper.removeEventListener(moveType, this.onMove);
+    this.wrapper.removeEventListener('mousemove', this.onMove);
     this.dist.finalPosition = this.dist.movePosition
   }
   addSlideEvents(){
     this.wrapper.addEventListener('mousedown', this.onStart);
-    this.wrapper.addEventListener('touchstart', this.onStart);
     this.wrapper.addEventListener('mouseup', this.onEnd);
-    this.wrapper.addEventListener('touchend', this.onEnd);
   }
 
   bindEvents(){
